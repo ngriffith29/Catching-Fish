b013bea3bc29294940e2b8a65c24e2bcb6f68928
Nathan Griffith <ngriffith29@student.egg.edu>
2018-11-15 09:22:11 -0500

    Updated front login page to work with ref inputs and setup react router to work with props.history.push

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from '../helpers'
+
 class StorePicker extends React.Component {
-    render() {
-        return <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" />
-        <button type="submit">Visit Store</button>
-        
-        
-        </form>
+    myInput= React.createRef();
+
+
+    goToStore = (event) => {
+    
+        //1. Stop form from submitting
+        event.preventDefault()
+        //2. Get teh text from that input
+      const storeName =  this.refs.myInput.value;
+        //3. Change the page to /store/whatever-they-entered.
+        this.props.history.push(`/store/${storeName}`)
+
     }
+  
+
+    render() {
+       
+        return (
+           <form className="store-selector" onSubmit={this.goToStore}>
+            <h2>Please Enter A Store</h2>
+            <input 
+            type="text" 
+            ref="myInput"
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()}
+            />
+            <button type="submit">Visit Store -></button>
+           
+           
+           
+           
+           
+           </form>
+
+
+
+        );
+
+
+
+    }
+
+
+
 }
 export default StorePicker;
